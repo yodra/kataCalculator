eb4f6ba4c0ec26864634f988dec3bf7a29e85893
Yodra <yodralopez@gmail.com>
2018-07-03 12:32:38 +0100

    green test suma_dos_numeros

--- src/main/java/es/leanmind/Calculate.java
+++ src/main/java/es/leanmind/Calculate.java
@@ -3,6 +3,17 @@ package es.leanmind;
 public class Calculate {
 
     public Integer sumStr(String numbers) {
-        return 1;
+        Integer result;
+
+        if (numbers.length() == 1) {
+            result = Integer.parseInt(numbers);
+        } else {
+            String valueOne = String.valueOf(numbers.charAt(0));
+            String valueTwo = String.valueOf(numbers.charAt(2));
+
+            result = Integer.parseInt(valueOne) + Integer.parseInt(valueTwo);
+        }
+
+        return result;
     }
 }
