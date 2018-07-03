2c3e1796dd08f0b2650cedafb1a51905f341c5e4
Yodra <yodralopez@gmail.com>
2018-07-03 13:51:18 +0100

    red test returns_the_same_number

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -10,4 +13,12 @@ public class CalculatorTest {
      * "//#;1#1" = 2
      */
 
+    @Test
+    public void returns_the_same_number() {
+        assertEquals(1, sumNumbers("1"));
+    }
+
+    private int sumNumbers(String expression) {
+        return 0;
+    }
 }
